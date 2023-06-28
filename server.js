164b4f8e7139982ea97ae5fd8f7350767978b02e
Emanuil Vartanyan <mike.vartanyan@yahoo.com>
2023-06-28 12:59:15 -0700

    added express endpoints

--- server.js
+++ server.js
@@ -1,32 +1,78 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+import express from "express";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const topic = "JavaScript";
-const question = "How to send an openai api request";
-const GPT35TurboMessage = [
-  { role: "system", content: `You are a ${topic} developer.` },
-  {
-    role: "user",
-    content: "Which npm package is best of openai api development?",
-  },
-  {
-    role: "assistant",
-    content: "The 'openai' Node.js library.",
-  },
-  { role: "user", content: question },
-];
 const openai = new OpenAIApi(configuration);
-let GPT3Turbo = async (message) => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: message,
-  });
 
-  return response.data.choices[0].message.content;
-};
-console.log("### I'm GPT-3.5-TURBO. ####", await GPT3Turbo(GPT35TurboMessage));
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from CodeX!",
+  });
+});
+app.post("/chatbot", async (req, res) => {
+  try {
+    const topic = "JavaScript";
+    const question = req.body.question;
+    const message = [
+      { role: "system", content: `You are a ${topic} developer.` },
+      {
+        role: "user",
+        content: "Which npm package is best of openai api development?",
+      },
+      {
+        role: "assistant",
+        content: "The 'openai' Node.js library.",
+      },
+      { role: "user", content: question },
+    ];
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: message,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].message.content,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
+
+// const topic = "JavaScript";
+// const question = "What are some good apps to build using the openai API?";
+// const GPT35TurboMessage = [
+//   { role: "system", content: `You are a ${topic} developer.` },
+//   {
+//     role: "user",
+//     content: "Which npm package is best of openai api development?",
+//   },
+//   {
+//     role: "assistant",
+//     content: "The 'openai' Node.js library.",
+//   },
+//   { role: "user", content: question },
+// ];
+// const openai = new OpenAIApi(configuration);
+// let GPT3Turbo = async (message) => {
+//   const response = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: message,
+//   });
+
+//   return response.data.choices[0].message.content;
+// };
+// console.log("### I'm GPT-3.5-TURBO. ####", await GPT3Turbo(GPT35TurboMessage));
+
+app.listen(8000, () =>
+  console.log("AI server started on http://localhost:8000")
+);
