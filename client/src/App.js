6b018dacab6ee637fa5a97655b95230bb937a7b7
Emanuil Vartanyan <mike.vartanyan@yahoo.com>
2023-06-28 22:32:22 -0700

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [data, setData] = useState("");
+  const [answer, setAnswer] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData(textInput);
+    makePost();
+    // Perform any desired actions with the submitted text
+  };
+
+  const payload = {
+    question: data,
+  };
+
+  const makePost = () => {
+    axios
+      .post("http://localhost:8000/chatbot", payload)
+      .then((response) => {
+        // Handle the response data
+        console.log(response.data);
+        setAnswer(response.data.bot);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
+
+  const handleChange = (e) => {
+    setTextInput(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1></h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={textInput}
+          onChange={handleChange}
+          placeholder="Enter your text"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h1>
+        {answer.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </h1>
     </div>
   );
 }
