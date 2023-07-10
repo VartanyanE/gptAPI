e8f5984db6b6759d2b797ea12037b1ad3fd4a0f9
VartanyanE <mike.vartanyan@yahoo.com>
2023-07-09 19:56:14 -0700

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,86 @@
-import React, { useState } from "react";
-import axios from "axios";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import "./App.css";
 
-function App() {
-  const [textInput, setTextInput] = useState("");
-  const [data, setData] = useState("");
-  const [answer, setAnswer] = useState([]);
+// function App() {
+//   const [textInput, setTextInput] = useState("");
+//   const [data, setData] = useState("");
+//   const [answer, setAnswer] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setData(textInput);
-    makePost();
-    // Perform any desired actions with the submitted text
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setData(textInput);
+//     makePost();
+//     // Perform any desired actions with the submitted text
+//   };
 
-  const payload = {
-    question: data,
-  };
+//   const payload = {
+//     question: data,
+//   };
 
-  const makePost = () => {
-    axios
-      .post("http://localhost:8000/chatbot", payload)
-      .then((response) => {
-        // Handle the response data
-        console.log(response.data);
-        setAnswer(response.data.bot);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error(error);
-      });
-  };
+//   const makePost = () => {
+//     axios
+//       .post("http://localhost:8000/chatbot", payload)
+//       .then((response) => {
+//         // Handle the response data
+//         console.log(response.data);
+//         setAnswer(response.data.bot);
+//       })
+//       .catch((error) => {
+//         // Handle any errors
+//         console.error(error);
+//       });
+//   };
 
-  const handleChange = (e) => {
-    setTextInput(e.target.value);
-  };
+//   const handleChange = (e) => {
+//     setTextInput(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <h1></h1>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={textInput}
+//           onChange={handleChange}
+//           placeholder="Enter your text"
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//       <h1>
+//         {/* {answer.map((fruit, index) => (
+//           <li key={index}>{fruit}</li>
+//         ))} */}
+//       </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import useForm from "./useForm";
+import "./App.css";
+
+const App = () => {
+  const { formData, handleChange, handleSubmit } = useForm();
 
   return (
     <div>
-      <h1></h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={textInput}
+          name="data"
+          value={formData.data}
           onChange={handleChange}
-          placeholder="Enter your text"
         />
         <button type="submit">Submit</button>
       </form>
-      <h1>
-        {/* {answer.map((fruit, index) => (
-          <li key={index}>{fruit}</li>
-        ))} */}
-      </h1>
+      <h1>{formData.response}</h1>
+      <h1 className="post">{formData.data}</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import axios from "./axios";
+import "./App.css";
+
+const useForm = () => {
+  const [formData, setFormData] = useState({ data: "", response: "" });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/chatbot", { data: formData.data });
+      setFormData({ ...formData, response: response.data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(formData);
+  return { formData, handleChange, handleSubmit };
+};
+
+export default useForm;
